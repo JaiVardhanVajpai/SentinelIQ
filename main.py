86524c8c587979ce9c3a76b375e34bfb6314bff5
JaiVardhanVajpai <jaivardhanvajpai06668@gmail.com>
2026-06-15 22:17:24 +0530

    update CORS with live Vercel URL

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:3001",
-        "https://sentineliq-frontend.vercel.app",
+        "https://sentinel-iq.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
